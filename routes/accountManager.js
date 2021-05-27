1a2497d6ac0e48c7d1ade42eba24064bf226a486
kaddy120 <32173574+kaddy120@users.noreply.github.com>
2021-05-27 19:12:10 +0200

    hash password

--- routes/accountManager.js
+++ routes/accountManager.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const { body, validationResult } = require('express-validator')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 router.get('/login', function (req, res, next) {
 
@@ -19,20 +21,28 @@ router.post('/signup',
   body('surname', 'Surname is required').notEmpty(),
   body('emailAddress', 'Enter a valid email address').isEmail(),
   body('password', 'password cannot be empty').notEmpty(),
-  body('password1', 'password do not match').equals(body.password),
+  body('password1').matches(body.password).withMessage('Password must match'),
 
   function (req, res, next) {
+    console.log(req.body.password)
+    console.log(req.body.password1)
     const errors = validationResult(req)
-    if (errors) {
+    if (errors.array().length !== 0) {
       return res.status(400).json({ errors: errors.array() })
+      // instead of returning a json i should return a page with error message.
     }
 
     const name = req.body.name
     const email = req.body.emailAddress
     const surname = req.body.surname
     const password = req.body.password
-    const password1 = req.body.password1
     const id = req.body.studentNumber
+
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      bcrypt.hash(password, salt, function (err, hash) {
+        // password = hash
+      })
+    })
 
     res.render('index')
   })
