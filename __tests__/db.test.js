6d59ab2762533859966885f486b2f4d4cc334df4
kaddy120 <32173574+kaddy120@users.noreply.github.com>
2021-05-31 21:18:51 +0200

    remove space from the body input

--- __tests__/db.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env jest */
-const { pools } = require('../db.js')
-
-test('Database has one object', async () => {
-  jest.setTimeout(30000)
-  const pool = await pools
-  const result = await pool.request().query('SELECT 1')
-  expect(result).toStrictEqual({ recordsets: [[{ '': 1 }]], recordset: [{ '': 1 }], output: {}, rowsAffected: [1] })
-})
