54b2620d93c5df30089324f99a0f2c0e9ea59fef
Clinton <clintonletsoela724c@gmail.com>
2021-06-16 02:21:10 +0200

    address validation automation

--- routes/addressField.js
+++ routes/addressField.js
@@ -1,30 +1,19 @@
-const axios = require('axios')
+const axios = require('axios');
 const express = require('express')
 const router = express.Router()
 
+
 router.get('/address', function (req, res, next) {
-    res.render('address', { title: 'Address' , body: req.body})
-    console.log(req.body)
+    res.render('address', { title: 'Fill in address'})
 })
 
+/*
 router.post('/address', async (req, res, next) => {
     const street = req.body.streetAddress
     const suburb = req.body.suburb
     const city = req.body.city
     const zipCode = req.body.zipCode
-   axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
-        params: {
-            address: `${street}, ${suburb}, ${city}, ${zipCode}`,
-            key: 'AIzaSyBQpS8Gb2C1coUXAIMFk-sTGmcclvup-GE'
-        }
-    }).then(response => { 
-        const address = response.data.results[0].formatted_address
-          console.log(address)
-        })
-        .catch(err => {
-            err = "Address not found, Enter a valid address"
-            console.log(err)
-        })
-})
+ 
+})*/
 
 module.exports = router
