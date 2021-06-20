177ff13d85045b05d9f987e605904488d26d0a62
Clinton <clintonletsoela724c@gmail.com>
2021-06-21 01:45:23 +0200

    slight modifications

--- services/groupJoinRequests.js
+++ services/groupJoinRequests.js
@@ -3,25 +3,32 @@ const votesRepository = container.resolve('votesRepository')
 const model = require('../models/voteValidation')
 var moment = require('moment')
 
-async function joinRequestExpiryDate () {
+async function joinRequestExpiryDate() {
     let requests = await votesRepository.getAllJoinRequests().then(result => { return result.recordset })
     let votes
     let getNumOfGroupMembers
     let voteCount
+    let counter
+    let requesteeGroups
     const filteredRequest = requests.filter(request => {
         return moment().diff(request.time_Stamp, 'hours') < 9
     })
 
-    for(var i = 0;i < filteredRequest.length;i++) {
-        votes = await votesRepository.getNumOfPeopleVoted(filteredRequest[i].requestId).then(result => {return result.recordset})
+    for (var i = 0; i < filteredRequest.length; i++) {
+        votes = await votesRepository.getNumOfPeopleVoted(filteredRequest[i].requestId).then(result => { return result.recordset })
         getNumOfGroupMembers = await votesRepository.getNumOfGroupMembers(filteredRequest[i].groupId)
-        if(votes[0].getNumOfPeopleVoted >= Math.floor(0.3*getNumOfGroupMembers)) {
+        if (votes[0].getNumOfPeopleVoted >= Math.floor(0.3 * getNumOfGroupMembers)) {
             voteCount = await votesRepository.countVotes(filteredRequest[i].requestId).then(result => { return result.recordset })
+            counter = model.countVotes(voteCount, getNumOfGroupMembers)
+            requesteeGroups = await votesRepository.getRequesteeGroups(filteredRequest[i].email).then(result => { return result.recordset })
+            if (counter === true && requesteeGroups.length < 10)
+                await votesRepository.acceptRequest(filteredRequest[i].email, filteredRequest[i].groupId)
+            await votesRepository.removeFromJoinRequests(filteredRequest[i].groupId)
         }
         else console.log("NOPE")
-       // await votesRepository.removeFromJoinRequests(filteredRequest[i].requestId)
+        await votesRepository.removeFromJoinRequests(filteredRequest[i].requestId)
     }
-    
+
     console.log(filteredRequest)
 }
 
