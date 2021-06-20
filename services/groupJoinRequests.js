346bd71223da081ed8dc40d11cc26b5a3fb952bc
Clinton <clintonletsoela724c@gmail.com>
2021-06-21 00:52:19 +0200

    filtering exprired join requests

--- services/groupJoinRequests.js
+++ services/groupJoinRequests.js
@@ -1,14 +1,20 @@
 const { container } = require('../di-setup')
 const votesRepository = container.resolve('votesRepository')
+var moment = require('moment')
 
 async function joinRequestExpiryDate () {
     const currentDate = await votesRepository.getCurrentDate().then(result => { return result.recordset })
     let requests = await votesRepository.getAllJoinRequests().then(result => { return result.recordset })
-    let expiryDate = []
-    for(var i = 0;i < requests.length;i++) {
-        expiryDate[i] = await votesRepository.getExpiryDate(requests[i].time_Stamp).then(result => { return result.recordset })
+    
+    const filteredRequest = requests.filter(request => {
+        return moment().diff(request.time_Stamp, 'hours') > 48
+    })
+
+    for(var i = 0;i < filteredRequest.length;i++) {
+        await votesRepository.removeFromJoinRequests(filteredRequest[i].requestId)
     }
-    console.log("HERE: ", expiryDate)
+    
+    console.log(filteredRequest)
 }
 
 module.exports = { joinRequestExpiryDate }
