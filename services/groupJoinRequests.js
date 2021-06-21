d85cf9d45a07670d3d101490b7d00d3d98463200
Clinton <clintonletsoela724c@gmail.com>
2021-06-21 02:46:42 +0200

    conditions for join requests complete

--- services/groupJoinRequests.js
+++ services/groupJoinRequests.js
@@ -14,19 +14,23 @@ async function joinRequestExpiryDate() {
         return moment().diff(request.time_Stamp, 'hours') < 9
     })
 
+    
     for (var i = 0; i < filteredRequest.length; i++) {
         votes = await votesRepository.getNumOfPeopleVoted(filteredRequest[i].requestId).then(result => { return result.recordset })
-        getNumOfGroupMembers = await votesRepository.getNumOfGroupMembers(filteredRequest[i].groupId)
-        if (votes[0].getNumOfPeopleVoted >= Math.floor(0.3 * getNumOfGroupMembers)) {
-            voteCount = await votesRepository.countVotes(filteredRequest[i].requestId).then(result => { return result.recordset })
+        getNumOfGroupMembers = await votesRepository.getNumOfGroupMembers(filteredRequest[i].requestId)
+        console.log(votes[0].numOfPeopleVoted)
+        if (votes[0].numOfPeopleVoted >= Math.floor(0.3 * getNumOfGroupMembers)) {
+           voteCount = await votesRepository.countVotes(filteredRequest[i].requestId).then(result => { return result.recordset })
             counter = model.countVotes(voteCount, getNumOfGroupMembers)
             requesteeGroups = await votesRepository.getRequesteeGroups(filteredRequest[i].email).then(result => { return result.recordset })
             if (counter === true && requesteeGroups.length < 10)
                 await votesRepository.acceptRequest(filteredRequest[i].email, filteredRequest[i].groupId)
             await votesRepository.removeFromJoinRequests(filteredRequest[i].groupId)
+            console.log("YEAH")
         }
-        else console.log("NOPE")
-        await votesRepository.removeFromJoinRequests(filteredRequest[i].requestId)
+        else{ console.log("NOPE")
+        //await votesRepository.removeFromJoinRequests(filteredRequest[i].requestId)
+       }
     }
 
     console.log(filteredRequest)
