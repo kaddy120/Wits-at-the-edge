2e0429da4eed902cf0072ece284bed7e9de6a4fa
Clinton <clintonletsoela724c@gmail.com>
2021-06-27 17:30:49 +0200

    meeting address suggestions implemented

--- public/javascript/meetingSelection.js
+++ public/javascript/meetingSelection.js
@@ -1,47 +1,25 @@
 'use strict'
 let num = 0
-const onlineButton = document.getElementById('cafe')
-//const faceToFaceButton = document.getElementById('face-to-face')
+const cafe = document.getElementById('cafe')
+const park = document.getElementById('park')
+const library = document.getElementById('library')
+const restaurant = document.getElementById('restaurant')
 
-console.log(onlineButton)
-onlineButton.addEventListener('click', (e) => {
-  document.getElementById('address').value = onlineButton.value
-  console.log("new address", document.getElementById('address').value)
- // console.log(onlineButton.value)
-})
-console.log(onlineButton.value)
-/*
-faceToFaceButton.addEventListener('click', async function (res, render) {
-  console.log("HERE")
-  res.render('locations')
-})
-/*
-faceToFaceButton.addEventListener('click', (e) => {
-  document.getElementById('submit').disabled = true
-  faceToFaceButton.style.background = '#e60000'
-  onlineButton.style.background = 'DodgerBlue'
-  const div = document.getElementById('address-div')
-  const addressLabel = document.createElement('label')
-  addressLabel.title = 'Meeting Address'
-  addressLabel.id = 'address-label'
-  addressLabel.innerText = 'Meeting Address'
-  const address = document.createElement('input')
-  address.className = 'form-control'
-  address.id = 'address'
-  address.name = 'address'
-  address.type = 'text'
-  if (num === 0) {
-    div.appendChild(addressLabel)
-    div.appendChild(address)
-    num++
-  }
-  const addresslister = document.getElementById('address')
 
-  addresslister.addEventListener('input', (e) => {
-    if (addresslister.value !== '') {
-      document.getElementById('submit').disabled = false
-    } else {
-      document.getElementById('submit').disabled = true
-    }
-  })
-})*/
+cafe.addEventListener('click', (e) => {
+  document.getElementById('address').value = cafe.value
+})
+
+park.addEventListener('click', (e) => {
+  document.getElementById('address').value = park.value
+})
+
+library.addEventListener('click', (e) => {
+  document.getElementById('address').value = library.value
+})
+
+restaurant.addEventListener('click', (e) => {
+  document.getElementById('address').value = restaurant.value
+})
+
+
