83d381aed894b86cf0347a3de4d8a78672b265a3
Luke Mohajane <gr8tluke@gmail.com>
2021-06-29 01:55:36 +0200

    Invite User adds users to the database, bug fixed

--- public/javascript/inviteUser.js
+++ public/javascript/inviteUser.js
@@ -6,10 +6,24 @@ for (let i = 0; i < list.length; i++) {
   list[i].style.backgroundColor = '#E8E8E8'
 }
 
-for (const i of list) {
-  i.children[1].lastChild.addEventListener('click', async function (e) {
+console.log(list)
+for (let i = 0; i < list.length; i++) {
+  console.log('I am in here')
+  list[i].children[1].firstChild.addEventListener('click', async function (e) {
     e.preventDefault()
     this.parentNode.parentNode.style.backgroundColor = '#E8E8E8'
     this.parentNode.parentNode.innerHTML = 'Invite Request Succesfully Sent'
+    const userId = this.nextSibling.nextSibling.value
+    const groupId = this.nextSibling.nextSibling.id
+    console.log(userId)
+    console.log(groupId)
+    try {
+      const response = await fetch(`/inviteUser/${userId}/${groupId}`, {
+        method: 'POST'
+      })
+      console.log('Completed!', response)
+    } catch (err) {
+      console.error(`Error: ${err}`)
+    }
   })
 }
--- routes/inviteUser.js
+++ routes/inviteUser.js
@@ -11,6 +11,7 @@ router.get('/inviteUser', function (req, res, next) {
 
 router.post('/inviteUser', async function (req, res, next) {
   // Get search filter inputs
+  console.log('I am here inside Invite User')
   const firstName = req.body.searchBarfName
   const surname = req.body.searchBarSurname
   const yearOfStudy = req.body.yearOfStudy
@@ -20,79 +21,64 @@ router.post('/inviteUser', async function (req, res, next) {
   if (evaluate === 0) {
     // Filter by name only
     const usersToInvite = await userRepo.getUserByfName(firstName)
-    if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 4 })
+    if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 3 })
     else res.redirect('/inviteUsers')
-    console.log(usersToInvite)
   } else if (evaluate === 1) {
     // Filter by Surname only
     const usersToInvite = await userRepo.getUserBySurname(surname)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else if (evaluate === 2) {
     // Filter by year Of Study Only
     const usersToInvite = await userRepo.getUserByYOS(yearOfStudy)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else if (evaluate === 3) {
     const usersToInvite = await userRepo.getUserBySchool(school)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
-    console.log(usersToInvite)
   } else if (evaluate === 4) {
     const usersToInvite = await userRepo.getUserByFullNameYOSschool(firstName, surname, yearOfStudy, school)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else if (evaluate === 12) {
     const usersToInvite = await userRepo.getUserByfNameSurname(firstName, surname)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else if (evaluate === 13) {
     const usersToInvite = await userRepo.getUserByfNameYOS(firstName, yearOfStudy)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else if (evaluate === 14) {
     const usersToInvite = await userRepo.getUserByfNameSchool(firstName, school)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else if (evaluate === 23) {
     const usersToInvite = await userRepo.getUserBySurnameYOS(surname, yearOfStudy)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else if (evaluate === 24) {
     const usersToInvite = await userRepo.getUserBySurnameSchool(surname, school)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else if (evaluate === 34) {
     const usersToInvite = await userRepo.getUserByYOSschool(yearOfStudy, school)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else if (evaluate === 123) {
     const usersToInvite = await userRepo.getUserByfNameSurnameYOS(firstName, surname, yearOfStudy)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else if (evaluate === 124) {
     const usersToInvite = await userRepo.getUserByfNameSurnameSchool(firstName, surname, school)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else if (evaluate === 134) {
     const usersToInvite = await userRepo.getUserByfNameYosSchool(firstName, yearOfStudy, school)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else if (evaluate === 234) {
     const usersToInvite = await userRepo.getUserBySurnameYosSchool(surname, yearOfStudy, school)
-    console.log(usersToInvite)
     if (usersToInvite.length > 0) res.render('inviteUserRequest', { users: usersToInvite, groupId: 58 })
     else res.redirect('/')
   } else {
@@ -102,10 +88,10 @@ router.post('/inviteUser', async function (req, res, next) {
 router.post('/inviteUser/:userId/:groupId', async function (req, res, next) {
   const userId = req.params.userId
   const groupId = req.params.groupId
+  console.log('Request Id', groupId)
   const response = null
   // Fix invite user
   await userRepo.addGroupJoinRequest(groupId, userId, response)
-  console.log('Request sent')
 })
 
 module.exports = router
