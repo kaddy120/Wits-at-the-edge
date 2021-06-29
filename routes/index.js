f937b0d1aa6b82b0fab899fedc7895a68fbad1f8
Mauz <mehtamaunas@gmail.com>
2021-06-29 10:36:34 +0200

    fixed bugs

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ const score = require('../models/score')
 const router = express.Router()
 /* GET home page. */
 router.get('/', async function (req, res, next) {
-  const user = req.user
-  const userScore = await score.getScore(user.email)
-  res.render('index', { title: 'Express', userScore: userScore })
+  res.render('index', { title: 'Express' })
 })
 
 module.exports = router
