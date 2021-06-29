25dec22777211637b2678c9bccd0be0e0e8c2986
kaddy120 <32173574+kaddy120@users.noreply.github.com>
2021-06-29 23:25:05 +0200

    chatting styling

--- app.js
+++ app.js
@@ -101,17 +101,18 @@ io.use((socket, next) => {
 })
 
 function sendMessage (socket, roomname) {
-  redisClient.lrange(roomname, '0', '-1', (err, data) => {
+  redisClient.lrange(roomname, '0', '100', (err, data) => {
     if (err) {
       console.log(err)
       return
     }
     data.map(x => {
       const usernameMessage = x.split(':')
-      const redisUsername = usernameMessage[0]
-      const redisMessage = usernameMessage[1]
+      const userName = usernameMessage[0]
+      const message = usernameMessage[1]
+      const timeSent = '12h00'
 
-      socket.emit('chat message', redisMessage
+      socket.emit('chat message', { userName, message, timeSent }
       )
     })
   })
@@ -127,8 +128,10 @@ io.on('connection', (socket) => {
   })
   // console.log(socket.request.session.passport.user)
   socket.on('chat message', (msg) => {
-    redisClient.rpush(roomname, `${userId}:${msg}`)
-    io.to(roomname).emit('chat message', msg)
+    const timeSent = new Date().toISOString().slice(0, 19).replace('T', ' ')
+    redisClient.rpush(roomname, `${userId}:${msg}:${timeSent}`)
+
+    io.to(roomname).emit('chat message', { userName: userId, message: msg, timeSent: '15:45' })
   })
 })
 
--- public/javascript/chat-frontend.js
+++ public/javascript/chat-frontend.js
@@ -16,8 +16,17 @@ form.addEventListener('submit', function (e) {
 socket.emit('join', { groupId })
 
 socket.on('chat message', function (msg) {
+  let class_ = 'msg-left'
+  if (msg.userName === 2) {
+    class_ = 'msg-right'
+  }
+  // class_ = 'msg-right'
+  const message = `<div class="${class_}">
+                   <div class="color-5">${msg.userName}</div>
+                   <div class="message">${msg.message}</div>
+                   <small>${msg.timeSent}</small>`
   const item = document.createElement('li')
-  item.textContent = msg
+  item.innerHTML = message
   messages.appendChild(item)
   window.scrollTo(0, document.body.scrollHeight)
 })
