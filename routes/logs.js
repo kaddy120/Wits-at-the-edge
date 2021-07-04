6dc31a5086665a0cc9497a2e47c06a52c53befb0
Mauz <mehtamaunas@gmail.com>
2021-07-04 03:10:08 +0200

    Bugs Fixed - All is working

--- routes/logs.js
+++ routes/logs.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const { body, validationResult } = require('express-validator')
+const { render } = require('pug')
 const router = express.Router()
 const { container } = require('../di-setup')
 const users = container.resolve('userRepository')
 
+router.get('/logs', async (req, res, next) => {
+  res.render('logs', { title: 'Search For User Logs' })
+})
 router.post('/logs',
   body('userEmail', 'Agenda cannot be empy').notEmpty(),
   body('start', 'Start date can not be null').notEmpty(),
@@ -19,3 +23,5 @@ router.post('/logs',
     const logs = await users.getUserLog(logger)
     res.render('logResults', { title: 'Log Results', logs: logs })
   })
+
+module.exports = router
